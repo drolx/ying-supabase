b4aae842a557dcf802b6bb8d692251525d090ddf
Godwin peter .O <me@godwin.dev>
2024-11-10 15:44:58 +0100

    Create auth.global.ts

---
+++ middleware/auth.global.ts
@@ -0,0 +1,10 @@
+export default defineNuxtRouteMiddleware((to, _from) => {
+    const session = useSupabaseSession()
+    const isAuthRoute = to.fullPath.startsWith('/auth') || to.meta.permission === false;
+
+    if (!session.value && !isAuthRoute) {
+      return navigateTo('/auth/login')
+    } else if(session.value && isAuthRoute) {
+        return navigateTo('/')
+    }
+})
