a35bc96141581edb3c087a9d528aa114332a400c
Godwin peter .O <me@godwin.dev>
2024-11-11 17:40:34 +0100

    split categories code to pinia store

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,19 +1,13 @@
 <script lang="ts" setup>
-import type { Database } from '~/supabase/database.types';
-import { useDate } from 'vuetify'
-import type { SortItem } from '~/types/shared';
-import { getPagingFilter } from '~/util/shared';
+import { useDate } from 'vuetify';
+import { storeToRefs } from 'pinia';
+import { useCategories } from '~/stores/categories';
 
-const date = useDate()
-const supabase = useSupabaseClient<Database>();
+const date = useDate();
+const categoriesStore = useCategories();
 
-const loading = ref(false);
-const search = ref('');
-const itemsPage = ref(1);
-const itemsPerPage = ref(10);
-const serverItems = ref<any[]>([]);
-const totalItems = ref(0);
-const sortBy = ref<SortItem[]>([])
+const { refreshData, createItem, loadItems, deleteItem } = categoriesStore;
+const { loading, search, createDialog, deleteDialog, createItemValue, sortBy, itemsPage, itemsPerPage, totalItems, serverItems } = storeToRefs(categoriesStore);
 
 const headers = [
     { title: 'ID', key: 'id' },
@@ -22,64 +16,6 @@ const headers = [
     { title: 'Description', key: 'description' },
     { title: 'Actions', key: 'actions', sortable: false },
 ];
-
-const loadItems = async (args: { page: number, itemsPerPage: number, sortBy: SortItem[] }) => {
-    loading.value = true
-    try {
-        const { from, to, ascending, filter } = getPagingFilter(args);
-        const { data, count, error } = await supabase.from('categories')
-            .select('*', { count: 'exact' })
-            .range(from, to)
-            .order(filter, { ascending })
-            .ilike('name', `%${search.value}%`)
-
-        if (error) throw error;
-        totalItems.value = count ?? 0;
-        serverItems.value = data;
-        loading.value = false;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const refreshData = () => loadItems({ page: itemsPage.value, itemsPerPage: itemsPerPage.value, sortBy: sortBy.value, });
-const createDialog = ref(false);
-const deleteDialog = ref(false);
-const createItemValue = reactive<{
-    name: string,
-    description: string | null,
-}>({
-    name: '',
-    description: null,
-});
-
-const createItem = async () => {
-    try {
-        const { error, data } = await supabase.from('categories').insert(createItemValue).select().single();
-        if (error) throw error;
-        if (data) {
-            createDialog.value = false;
-            refreshData();
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-const deleteItem = async (value: any) => {
-    try {
-        if (value?.id) {
-            const { error, count } = await supabase.from('categories')
-                .delete({ count: 'exact' })
-                .eq('id', value.id)
-
-            if (error) throw error;
-            refreshData();
-            deleteDialog.value = false;
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
 </script>
 
 <template>
